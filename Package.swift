18d031d53d1757c014d628a15ce0a98fe50e9a7d
Prn-Ice <stormprince77@gmail.com>
2023-07-01 15:04:52 +0100

    update butter cms manager dependency

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.76.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
-        .package(url: "https://github.com/Prn-Ice/buttercms-swift.git", from: "1.0.9"),
+        .package(url: "https://github.com/Prn-Ice/buttercms-swift.git", from: "1.1.2"),
     ],
     targets: [
         .executableTarget(
@@ -18,18 +18,18 @@ let package = Package(
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "Leaf", package: "leaf"),
-                .product(name: "ButterCMSSDK", package: "buttercms-swift")
+                .product(name: "ButterCMSSDK", package: "buttercms-swift"),
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
                 // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
                 // builds. See <https://www.swift.org/server/guides/building.html#building-for-production> for details.
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
             ]
         ),
         .testTarget(name: "AppTests", dependencies: [
             .target(name: "App"),
             .product(name: "XCTVapor", package: "vapor"),
-        ])
+        ]),
     ]
 )
