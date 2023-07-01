1eefa83e45b540296bd3105fe00ab806a0f8498e
Prn-Ice <stormprince77@gmail.com>
2023-07-01 14:14:27 +0100

    update butter cms manager dependency

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.76.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
-        .package(url: "https://github.com/ButterCMS/buttercms-swift.git", from: "1.0.7"),
+        .package(url: "https://github.com/Prn-Ice/buttercms-swift.git", from: "1.0.9"),
     ],
     targets: [
         .executableTarget(
