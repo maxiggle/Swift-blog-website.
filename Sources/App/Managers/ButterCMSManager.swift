1cc873245741d90649389cbd4b09372f2b37c8a8
Prn-Ice <stormprince77@gmail.com>
2023-07-01 12:32:50 +0100

    add butter cms manager

---
+++ Sources/App/Managers/ButterCMSManager.swift
@@ -0,0 +1,86 @@
+//
+//  ButterCMSManager.swift
+//  ButterCMSSample
+//
+//  Created by Martin Srb on 17.09.2021.
+//
+
+import ButterCMSSDK
+import Combine
+
+class ButterCMSManager {
+    static var shared = ButterCMSManager()
+    let butter = ButterCMSClient(apiKey: "YOUR-TOKEN-HERE")
+    let homePageSubject = PassthroughSubject<PageResponse<HomePageFields>, Error>()
+    let caseStudyPagesSubject = PassthroughSubject<PagesResponse<CaseStudyPageFields>, Error>()
+    let caseStudyPageSubject = PassthroughSubject<PageResponse<CaseStudyPageFields>, Error>()
+    let faqCollectionSubject = PassthroughSubject<CollectionResponse<FaqCollectionItem>, Error>()
+    let blogSubject = PassthroughSubject<PostsResponse, Error>()
+    let postSubject = PassthroughSubject<PostResponse, Error>()
+
+    func getHomePage() {
+        butter.getPage(slug: "homepage", parameters: [.locale(value: "en")], pageTypeSlug: "homepage", type: HomePageFields.self) { result in
+            switch result {
+            case let .success(page):
+                self.homePageSubject.send(page)
+            case let .failure(error):
+                self.homePageSubject.send(completion: .failure(error))
+            }
+        }
+    }
+
+    func getPosts() {
+        butter.getPosts(parameters: [.excludeBody]) { result in
+            switch result {
+            case let .success(posts):
+                self.blogSubject.send(posts)
+            case let .failure(error):
+                self.blogSubject.send(completion: .failure(error))
+            }
+        }
+    }
+
+    func getPost(slug: String) {
+        butter.getPost(slug: slug) { result in
+            switch result {
+            case let .success(post):
+                self.postSubject.send(post)
+            case let .failure(error):
+                self.postSubject.send(completion: .failure(error))
+            }
+        }
+    }
+
+    func getPages() {
+        butter.getPages(parameters: [.locale(value: "en")], pageTypeSlug: "case_studies", type: CaseStudyPageFields.self) { result in
+            switch result {
+            case let .success(pages):
+                self.caseStudyPagesSubject.send(pages)
+            case let .failure(error):
+                self.caseStudyPagesSubject.send(completion: .failure(error))
+            }
+        }
+    }
+
+    func getPage(slug: String) {
+        butter.getPage(slug: slug, parameters: [.locale(value: "en")], pageTypeSlug: "case_studies", type: CaseStudyPageFields.self) { result in
+            switch result {
+            case let .success(page):
+                self.caseStudyPageSubject.send(page)
+            case let .failure(error):
+                self.caseStudyPageSubject.send(completion: .failure(error))
+            }
+        }
+    }
+
+    func getCollection() {
+        butter.getCollection(slug: "faq", parameters: [.locale(value: "en")], type: FaqCollectionItem.self) { result in
+            switch result {
+            case let .success(items):
+                self.faqCollectionSubject.send(items)
+            case let .failure(error):
+                self.faqCollectionSubject.send(completion: .failure(error))
+            }
+        }
+    }
+}
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,15 +1,15 @@
-import Vapor
 import Leaf
+import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
-//    app.middleware = init()
+    //    app.middleware = init()
     let files = FileMiddleware(publicDirectory: app.directory.publicDirectory)
-     app.middleware.use(files)
-    
+    app.middleware.use(files)
+
     app.views.use(.leaf)
-    
+
     // register routes
     try routes(app)
 }
